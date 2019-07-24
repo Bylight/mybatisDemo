cb6ed46ca439227f672e82e564e89a2b6356ac3b
Bylight <hejiaweicn@163.com>
2019-07-24 16:35:25 +0800

    complete base operation

---
+++ src/main/java/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package mapper;
+
+import entity.User;
+
+public interface UserMapper {
+    User queryUserById(int id);
+}
--- src/main/java/mybatis/MyBatisDemo.java
+++ src/main/java/mybatis/MyBatisDemo.java
@@ -1,6 +1,7 @@
 package mybatis;
 
 import entity.User;
+import mapper.UserMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -14,7 +15,7 @@ import java.io.InputStream;
  * @author 何家伟
  */
 public class MyBatisDemo {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         /**
          * 第一步：要先获取 SqlSessionFactory， MyBatis 的每一个操作都以 SqlSessionFactory 为核心
          *
@@ -29,25 +30,34 @@ public class MyBatisDemo {
         // 在这里使用 Resources 从指定 classpath 加载了 xml 配置文件
         InputStream resourceAsStream = MyBatisDemo.class.getClassLoader().getResourceAsStream(xmlConfigResource);
         // 构建sqlSession的工厂
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
 
-        //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
-        //Reader reader = Resources.getResourceAsReader(xmlConfigResource);
-        //构建sqlSession的工厂
-        //SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-
-        //创建能执行映射文件中sql的sqlSession
-        SqlSession session = sessionFactory.openSession();
         /**
-         * 映射sql的标识字符串，
-         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
-         * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         * 有了 session 工厂后就可以创建 session 实例，然后使用 session 实例来使用映射器(map)执行 sql 语句了
+         * 同样有两种方式，但我们强烈推荐后面的方法
+         * 使用正确描述每个语句的参数和返回值的接口（比如 BlogMapper.class）
+         * 不仅可以执行更清晰和类型安全的代码，而且还不用担心易错的字符串字面值以及强制类型转换。
          */
-        String statement = "userMapper.getUser";//映射sql的标识字符串
-        //执行查询返回一个唯一user对象的sql
-        User user = session.selectOne(statement, 2);
-        System.out.println(user);
-        resourceAsStream.close();
+        // 不推荐的方法
+        /*
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            // 映射sql的标识字符串
+            String statement = "UserMapper.queryUserById";
+            // 执行查询返回一个唯一user对象的sql
+            User user = session.selectOne(statement, 2);
+            System.out.println(user);
+        }
+        */
+
+        // 第二种方法有很多优势
+        // 首先它不依赖于字符串字面值，会更安全一点；
+        // 其次，如果你的 IDE 有代码补全功能，那么代码补全可以帮你快速选择已映射的 SQL 语句。
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            User user = mapper.queryUserById(2);
+            System.out.println(user);
+        }
+
     }
 }
 
